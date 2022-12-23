1275127a9c2845a5773c1d81345ef1b88ac3e96a
maxhirtens <max@maxhirtenstein.com>
2022-12-23 00:45:36 -0800

    finished

--- step3.js
+++ step3.js
@@ -1,25 +1,53 @@
 const fs = require('fs');
 const axios = require('axios');
 
-function cat(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
+// print out text from file.
+function cat(data, out) {
+  fs.readFile(data, 'utf8', (err, text) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    console.log(`Data in file: ${data}`)
+    output(text, out);
+    console.log(text);
   })
 }
 
-async function webCat(path) {
-  let url = await axios.get(path);
-  console.log(url.data)
+// print out text from url.
+async function webCat(data, out) {
+  let text = await axios.get(data);
+  output(text.data, out);
+  console.log(text.data);
 }
 
-let path = process.argv[2];
+// write text from url or file to new file.
+function output(data, out) {
+  if (out) {
+    fs.writeFile(out, data, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        console.log(data);
+      }
+    }
+    )
+  }
+};
 
-if (path.startsWith('http')) {
-  webCat(process.argv[2]);
+let data;
+let out;
+
+if (process.argv[2] === '--out') {
+  console.log('WRITING');
+  data = process.argv[3];
+  out = process.argv[4];
 } else {
-  cat(process.argv[2]);
+  data = process.argv[2];
+}
+
+if (data.startsWith('http')) {
+  webCat(data, out);
+} else {
+  cat(data, out);
 }
